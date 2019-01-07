ed8e843fe3c133d6885dd5c35c9da6edcb1360c1
bravoactual <dankchoice@gmail.com>
2019-01-07 11:39:34 -0800

    Updaate Code ISA.py
    updated birthday selector and cash selector

--- Code ISA.py
+++ Code ISA.py
@@ -42,8 +42,7 @@ driver.find_element_by_css_selector(add_to_cart_css_selector).click()
 sleep(2)
 
 # Select Pick Up
-driver.find_element_by_css_selector(
-    'body > div:nth-child(22) > div > div > div.styles__ButtonContainer-sc-1edagdx-2.ghJraX.sc-bwzfXH.hKiLMS.sc-bdVaJa.gMYdSF').click()
+driver.find_element_by_css_selector('body > div:nth-child(22) > div > div > div.styles__ButtonContainer-sc-1edagdx-2.ghJraX.sc-bwzfXH.hKiLMS.sc-bdVaJa.gMYdSF').click()
 sleep(2)
 
 # click on cart
@@ -51,8 +50,7 @@ driver.find_element_by_css_selector(
     '#navigation-container > div > div.navigation__UserCartContainer-sc-18zpigd-9.jqRiWr.sc-bwzfXH.kkVFCe.sc-bdVaJa.iHZvIS > div.icon__CartIcon-sc-4qy5kh-0.eoUoeu.sc-bdVaJa.iHZvIS').click()
 sleep(2)
 # proceed to checkout
-driver.find_element_by_xpath(
-    '//button[text()="Proceed to Checkout - $"]').click()
+driver.find_element_by_xpath('//button[text()="Proceed to Checkout - $"]').click()
 sleep(2)
 # Checkout as guest
 driver.find_element_by_xpath('/html/body/div[6]/div/div/div[5]/a').click()
@@ -73,15 +71,15 @@ Phone_nu_css_selector = '.text-input-k2ughi-0-Component'
 Phone_nu = driver.find_element_by_css_selector(Phone_nu_css_selector)
 Phone_nu.send_keys('1234567891 ')
 # Birth day
-month_xpath = '//[@id="top-scroll-container"]/div[2]/div/div[1]/div[1]/div[2]/div[2]/div[3]/div[2]/div[1]/span/select'
+month_xpath = '//*[@id="top-scroll-container"]/div[2]/div/div[1]/div[1]/div[2]/div[2]/div[3]/div[2]/div[1]/span/select'
 select_month = Select(driver.find_element_by_xpath(month_xpath))
 select_month.select_by_value('12')
-day_xpath = '//[@id="top-scroll-container"]/div[2]/div/div[1]/div[1]/div[2]/div[2]/div[3]/div[2]/div[2]/span/select'
+day_xpath = '//*[@id="top-scroll-container"]/div[2]/div/div[1]/div[1]/div[2]/div[2]/div[3]/div[2]/div[2]/span/select'
 select_day = Select(driver.find_element_by_xpath(day_xpath))
 select_day.select_by_value('17')
-year_xpath = '//[@id="top-scroll-container"]/div[2]/div/div[1]/div[1]/div[2]/div[2]/div[3]/div[2]/div[3]/span/select'
+year_xpath = '//*[@id="top-scroll-container"]/div[2]/div/div[1]/div[1]/div[2]/div[2]/div[3]/div[2]/div[3]/span/select'
 select_year = Select(driver.find_element_by_xpath(year_xpath))
 select_year.select_by_value('2018')
 # choosing Credit
-choosing_cash_box_xpath = '//[@id="top-scroll-container"]/div[2]/div/div[1]/div[1]/div[4]/div[2]/div/div[1]/label[1]/span'
+choosing_cash_box_xpath = '//*[@id="top-scroll-container"]/div[2]/div/div[1]/div[1]/div[4]/div[2]/div/div[1]/label[2]/input'
 driver.find_element_by_xpath(choosing_cash_box_xpath).click()
